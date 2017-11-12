2909c4fdfc86a08fc324be7ec1b546a856997f0a
Hemang Rajyaguru <hemdagem@gmail.com>
2017-11-12 13:28:04 +0000

    added conditional rendering and form validation

--- src/Nooptime.Web/wwwroot/js/site.js
+++ src/Nooptime.Web/wwwroot/js/site.js
@@ -1,5 +1,7 @@
 ﻿document.addEventListener("DOMContentLoaded",
     function (event) {
+
+
         var vm = new Vue(
             {
                 el: "#vue-instance",
@@ -11,6 +13,7 @@
 
                     axios.get("/api/UptimeCheck/GetAll")
                         .then(function (response) {
+
                             // Replace the data
                             // See https://vuejs.org/v2/guide/list.html#Replacing-an-Array
                             that.items = that.items.concat(response.data);
@@ -22,21 +25,24 @@
                 methods: {
                     add: function () {
                         var that = this;
-                        var model = {
-                            Id: "00000000-0000-0000-0000-000000000000",
-                            Name: document.getElementById("name").value,
-                            Description: document.getElementById("description").value,
-                            Interval: document.getElementById("interval").value
-                        };
 
-                        axios.post("/api/UptimeCheck/Post", model)
-                            .then(function (response) {
-                                that.items.push(model);
-                                console.log(response);
-                            })
-                            .catch(function (error) {
-                                console.log(error);
-                            });
+                        if (document.getElementsByTagName("form")[0].checkValidity()) {
+                            var model = {
+                                Id: "00000000-0000-0000-0000-000000000000",
+                                Name: document.getElementById("name").value,
+                                Description: document.getElementById("description").value,
+                                Interval: document.getElementById("interval").value
+                            };
+
+                            axios.post("/api/UptimeCheck/Post", model)
+                                .then(function (response) {
+                                    that.items.push(model);
+                                    console.log(response);
+                                })
+                                .catch(function (error) {
+                                    console.log(error);
+                                });
+                        }
                     },
 
                     remove: function (item, index) {
