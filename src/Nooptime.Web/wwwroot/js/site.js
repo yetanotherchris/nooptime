d72abfa7b0e8fdd9562806099f199745851f6779
Hemang <hemang.rajyaguru@totaljobsgroup.com>
2017-11-17 14:10:15 +0000

    fixed issues with rendering view. api calls disabled in site.js

--- src/Nooptime.Web/wwwroot/js/site.js
+++ src/Nooptime.Web/wwwroot/js/site.js
@@ -1,7 +1,6 @@
 ﻿document.addEventListener("DOMContentLoaded",
     function (event) {
 
-
         var vm = new Vue(
             {
                 el: "#vue-instance",
@@ -11,16 +10,16 @@
                 created: function () {
                     var that = this;
 
-                    axios.get("/api/UptimeCheck/GetAll")
-                        .then(function (response) {
+                    //axios.get("/api/UptimeCheck/GetAll")
+                    //    .then(function (response) {
 
-                            // Replace the data
-                            // See https://vuejs.org/v2/guide/list.html#Replacing-an-Array
-                            that.items = that.items.concat(response.data);
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
+                    //        // Replace the data
+                    //        // See https://vuejs.org/v2/guide/list.html#Replacing-an-Array
+                    //        that.items = that.items.concat(response.data);
+                    //    })
+                    //    .catch(function (error) {
+                    //        console.log(error);
+                    //    });
                 },
                 methods: {
                     add: function () {
@@ -33,52 +32,30 @@
                                 Description: document.getElementById("description").value,
                                 Interval: document.getElementById("interval").value
                             };
-
-                            axios.post("/api/UptimeCheck/Post", model)
-                                .then(function (response) {
-                                    that.items.push(model);
-                                    console.log(response);
-                                })
-                                .catch(function (error) {
-                                    console.log(error);
-                                });
+                            that.items.push(model);
+                            //axios.post("/api/UptimeCheck/Post", model)
+                            //    .then(function (response) {
+                                    
+                            //        console.log(response);
+                            //    })
+                            //    .catch(function (error) {
+                            //        console.log(error);
+                            //    });
                         }
                     },
 
                     remove: function (item, index) {
                         var that = this;
+                        that.items.splice(index, 1);
+                        //axios.delete("/api/UptimeCheck/Delete", { params: { id: item.id } })
+                        //    .then(function (response) {
+                               
+                        //        console.log(response);
+                        //    })
+                        //    .catch(function (error) {
+                        //        console.log(error);
 
-                        axios.delete("/api/UptimeCheck/Delete", { params: { id: item.id } })
-                            .then(function (response) {
-                                that.items.splice(index, 1);
-                                console.log(response);
-                            })
-                            .catch(function (error) {
-                                console.log(error);
-
-                            });
-                    },
-
-                    edit: function(item, index) {
-                        var that = this;
-
-                        if (document.getElementById("editForm").checkValidity()) {
-                            var model = {
-                                Id: "00000000-0000-0000-0000-000000000000",
-                                Name: document.getElementById("name").value,
-                                Description: document.getElementById("description").value,
-                                Interval: document.getElementById("interval").value
-                            };
-
-                            axios.post("/api/UptimeCheck/Patch", model)
-                                .then(function (response) {
-                                    that.items.push(model);
-                                    console.log(response);
-                                })
-                                .catch(function (error) {
-                                    console.log(error);
-                                });
-                        }
+                        //    });
                     }
                 }
             });
