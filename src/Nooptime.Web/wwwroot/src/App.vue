1f382e728c78d9635e8f892ed46e6e1821e27b87
Chris S <chrisasmall+github@gmail.com>
2017-11-19 00:25:42 +0000

    Missed the .vue files before

--- src/Nooptime.Web/wwwroot/src/App.vue
+++ src/Nooptime.Web/wwwroot/src/App.vue
@@ -1,32 +1,17 @@
 <template>
-  <div id="app">
-    <img src="http://vuejs.org/images/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div id="app">
+		<h1>nooptime!</h1>
+		<UptimeCheckList/>
+	</div>
 </template>
 
 <script>
+import UptimeCheckList from './components/UptimeCheckList.vue'
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+	components: {
+		UptimeCheckList
+	}
 }
 </script>
 
---
+++ src/Nooptime.Web/wwwroot/src/components/UptimeCheckItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    {{ item.text }}
+    <button @click="$emit('remove', item.id)">
+      X
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
