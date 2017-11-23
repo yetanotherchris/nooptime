71a45d60fa489fc32e6351a7e818af358df33efd
Chris S <chrisasmall+github@gmail.com>
2017-11-23 18:48:08 +0000

    More UI mocks with Vue

---
+++ src/Nooptime.Web/wwwroot/src/Manage.vue
@@ -0,0 +1,72 @@
+<template>
+	<div>
+		<div class="row">
+			<div class="col-md-12">
+				<button class="btn btn-primary pull-right" @click="show = !show">New uptime check</button>
+			</div>
+			<div class="col-md-10">&nbsp;</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>All items</h4>
+				<UptimeCheckList v-if="!show"/>
+				<transition name="bounce">
+					<AddEditForm v-if="show"/>
+				</transition>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import UptimeCheckList from "./components/manage/UptimeCheckList.vue";
+import AddEditForm from "./components/manage/AddEditForm.vue";
+
+export default {
+	components: {
+		UptimeCheckList,
+		AddEditForm
+	},
+	props: {
+		show: {
+			type: Boolean,
+			required: true
+		}
+	}
+};
+</script>
+
+<style scoped>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+img {
+	width: 200px;
+	height: 200px;
+}
+
+h1,
+h2 {
+	font-weight: normal;
+}
+
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+
+a {
+	color: #42b983;
+}
+</style>
---
+++ src/Nooptime.Web/wwwroot/src/components/manage/AddEditForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="well">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+          <h4 class="modal-title">Contact</h4>
+        </div>
+        <div class="modal-body">
+          <p>Feel free to contact us for any issues you might have with our products.</p>
+          <div class="row">
+            <div class="col-xs-6">
+              <label>Name</label>
+              <input type="text" class="form-control" placeholder="Name">
+            </div>
+            <div class="col-xs-6">
+              <label>Email</label>
+              <input type="text" class="form-control" placeholder="Email">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12">
+              <label>Message</label>
+              <textarea class="form-control" rows="3">Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac</textarea>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-success">Send</button>
+        </div>
+  </div>
+</template>
