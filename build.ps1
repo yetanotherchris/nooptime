68cfcd15f5ae4fb7150c22543883290a2799af5d
Chris S <chrisasmall+github@gmail.com>
2017-12-05 22:38:45 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+
 choco install nodejs
 
 ###############################################################################
