d40478b52af67f5052da4143601cf2889067ab06
Chris S <chrisasmall+github@gmail.com>
2017-12-13 10:33:42 +0000

    Added appsettings.json for configuration

--- src/Nooptime.Web/Startup.cs
+++ src/Nooptime.Web/Startup.cs
@@ -18,6 +18,8 @@ namespace Nooptime.Web
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
 
             Configuration = builder.Build();
